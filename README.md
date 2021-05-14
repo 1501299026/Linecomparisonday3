4730de55702ab075b32a6465f2e6e8113d8532d3
1501299026 <sinumsd98@gmail.com>
2021-05-14 10:22:21 -0700

    ADDED :Welcome message added

---
+++ README.md
@@ -0,0 +1,1 @@
+# Linecomparisonday3
---
+++ src/com/linecomparison/LineComparison.java
@@ -0,0 +1,11 @@
+package com.linecomparison;
+
+public class LineComparison {
+public static void main(String[] args) {
+
+ System.out.println("welcome to line comparison");
+	
+
+}
+}
+
