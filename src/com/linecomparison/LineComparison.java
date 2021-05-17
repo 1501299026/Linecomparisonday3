2898f99b78fc4445df8c61e2a023b9308c99043b
1501299026 <sinumsd98@gmail.com>
2021-05-17 09:51:40 -0700

    ADDED: Opps concept added

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -6,11 +6,23 @@ public static void main(String[] args) {
 
 	
 	System.out.println("Welcome to Line Comparision Program");
-	
+
+	double line1,line2,difference;
+	System.out.println("Getting Coordinates of Line 1");
 	getPoints();
+	line1=calculateLength(x1, y1, x2, y2);
 	
-	System.out.println("The Length of the Line is "+calculateLength(x1, y1, x2, y2));
+	System.out.println("Getting Coordinates of Line 2");
+	getPoints();
+	line2=calculateLength(x1, y1, x2, y2);
 	
+	difference=line1-line2;
+	if(difference==0)
+		System.out.println("Both The Lines Are EQUAL In Length!!");
+	else if(difference>0)
+		System.out.println("Line1 is Greater Than Line2 ");
+	else
+		System.out.println("Line1 is Smaller Than Line2 ");
 }
 
 static void getPoints()
@@ -23,7 +35,7 @@ static void getPoints()
 	x2=scan.nextInt();
 	y2=scan.nextInt();
 	
-	System.out.println("These are the coordinates ("+x1+","+y1+") and ("+x2+","+y2+")");
+
 }
 
 static double calculateLength(int x1,int y1,int x2,int y2)
@@ -31,9 +43,8 @@ static double calculateLength(int x1,int y1,int x2,int y2)
 	
 	double leng;
 	leng=Math.pow(Math.pow(x2-x1,2) + Math.pow(y2-y1,2),0.5);
+	System.out.println("\nThese are the coordinates ("+x1+","+y1+") and ("+x2+","+y2+") and the Length is "+leng+"\n");
 	return leng;
-	
-
 }
 }
 
